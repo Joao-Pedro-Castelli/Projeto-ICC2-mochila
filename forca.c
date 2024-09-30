70ebf36ed8a9057aa6b4f96bf79148d2144bf682
Joao Pedro <jotapcastelli@gmail.com>
2024-09-30 18:22:07 -0300

    comentei forca.c

--- forca.c
+++ forca.c
@@ -11,11 +11,13 @@ void forcaBruta(ITEM **v, int tamVetor, int *bitMask, int camada, int *auxValor,
     int peso = 0;
     int valor = 0;
 
-
+    //se a camada de recursao eh a ultima, ou seja estou na ultima folha, ao inves de entrar mais na recursao,
+    //vou calcular o bitMask que foi definido e ver se eh o melhor ate agora
     if(camada == tamVetor){
 
         for(int i = 0; i < tamVetor; i++){
-		    if((*bitMask & ((int) pow(2, i))) == ((int) pow(2, i))){
+		    if((*bitMask & ((int) pow(2, i))) == ((int) pow(2, i))){ //bitMask precisa ser um ponteiro para as mudancas se
+									     //propagarem pelas funcoes
 
                 peso += getPeso(v[i]);
                 valor += getValor(v[i]);
@@ -42,14 +44,15 @@ void forcaBruta(ITEM **v, int tamVetor, int *bitMask, int camada, int *auxValor,
 
 
 
-    int op = (int)pow(2,camada);
+    int op = (int)pow(2,camada); //coloco o bit daquela camada como i
 
-    *bitMask = *bitMask & (~op);
+    *bitMask = *bitMask & (~op); //not op significa que vou tirar, colocar como 0 o bit, daquela camada, o item i com i = camada
     
     forcaBruta(v, tamVetor, bitMask, camada+1, auxValor, auxBitMask, pesoMax);
 
-    *bitMask = *bitMask | op;
+    *bitMask = *bitMask | op; //agora vamos calcular o bitMask mas com o bit daquela camada como 1, com o item na mochila
 
     forcaBruta(v, tamVetor, bitMask, camada+1, auxValor, auxBitMask, pesoMax);
     
+    return;
 }
